2003efbdc1cfb4fd215e9cd8280cff9352f71f63
javatrainee1 <varshakhamkar693@gmail.com>
2022-01-14 01:31:14 -0800

    My Project
    14 Jan 22

---
+++ MiniProject/GuessGame.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class GuessGame {
+	
+	public static void main(String[] args) {
+		
+		int secretNum;
+		int guess;
+		boolean correct = false;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("GIVE ME SECRET NUMBER:");
+		secretNum = keyboard.nextInt();
+		
+		while (!correct) {
+			
+			System.out.print("GUESS: ");
+			guess = keyboard.nextInt();
+			
+			if (guess == secretNum) {
+				correct = true;
+				System.out.println("You are right!");
+				
+			}
+			
+			else if (guess < secretNum) {
+				System.out.println("HIGHER");
+			}
+			else if (guess < secretNum) {
+				System.out.println("LOWER");
+			
+			
+			}
+		}
+	}
+}
+		
+		
+
+		
+	
+
+
